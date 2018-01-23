a9e289843af4dbb4bb9e773e17ed0672fadfe55e
Hendrix Roa <hendrix.alex87@gmail.com>
2018-01-23 00:44:40 -0400

    agregado archivo de test para methodos publicos

--- tests.js
+++ tests.js
@@ -1,8 +1,34 @@
-'use strict'
+const assert = require('chai').assert
 const api = require('./poloniex-client')
-api.determinePriceCurrency('BTC_NXT', 'buy', 333)
-.then(res =>{
-    console.log(res)
-}, err =>{
-    console.log(err)
+
+describe('Test poloniex api public', async () => {
+	it('Should be return currencies in a object', () => {
+		const currenciesAvailable = api.currenciesAvailable()
+		currenciesAvailable.then( currencies =>{
+			assert.isObject(currencies)
+		}).catch( err =>{
+		})	
+	})
+
+	it('Should be return Chart data as object and not empty', () => {
+		const params = { 
+			currencyPair: 'BTC_LTC',
+			period: 14400,
+			start: 1513569600,
+			end: 1513656000 
+		}
+		const chartData = api.returnChartData(params)
+		chartData.then( chart =>{
+			assert.isNotEmpty(chart)
+		}).catch(err =>{
+		})
+	})
+
+	it('Should be return order book for all currencies', () => {
+		const ordersBook = api.returnOrderBook('all')
+		ordersBook.then( orders =>{
+			assert.isObject(orders)
+		}).catch( err =>{
+		})	
+	})
 })
